abdcaa9c3da444c6c0140f080c0223d023e52b0b
markmezei <94128834+markmezei@users.noreply.github.com>
2023-02-05 22:16:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+import fetch from "node-fetch";
+import figlet from "figlet";
+import chalk from 'chalk';
+import {table} from 'table';
+
+
+async function fetchData(){
+    const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+    const header = {headers: {'X-CMC_PRO_API_KEY': '540bb0e5-c206-45f9-b23f-c2634e202b63'}};
+    try{
+        figlet("Cryptocurrency Prices", (error,data) => {
+            (error) ? console.error(error) : console.info(chalk.green(data));
+        })
+        const response = await fetch(url,header);
+        const data = await response.json();
+        displayData(data.data);
+    }catch(e){
+        console.error(e);
+    }
+}
+fetchData();
+
+
+
+function displayData(cryptocurrencies){
+    const display = () => {
+        let data = [];
+        const header = new Array('Name', 'Symbol','Price', '1h %','24h %','7d %', 'Volume 24h', 'Volume change 24h');
+        data.push(header);
+        cryptocurrencies.forEach((element, index) => {
+            if(index >= 20) {
+                return;
+            }
+            data.push([
+                chalk.green(element.name), 
+                chalk.magenta(element.symbol),
+                chalk.red(`$${element.quote.USD.price.toFixed(3)}`),
+                chalk.blueBright(`$${element.quote.USD.percent_change_1h.toFixed(3)}`),
+                chalk.blueBright(`$${element.quote.USD.percent_change_24h.toFixed(3)}`),
+                chalk.blueBright(`$${element.quote.USD.percent_change_7d.toFixed(3)}`),
+                chalk.yellow(`$${element.quote.USD.volume_24h.toFixed(3)}`),
+                chalk.yellow(`$${element.quote.USD.volume_change_24h.toFixed(3)}`),
+            ]);
+        });
+        const config = {
+            columnDefault: {
+              width: 18,
+              alignment:"center"
+            }
+        };
+        console.log(table(data, config));
+    }
+    display();
+}
